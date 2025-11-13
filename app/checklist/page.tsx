1ffab81f43e9311501d8c0874d3ba4f12e733377
nick301024 <nick_bucher@sluz.ch>
2025-11-13 09:22:58 +0100

    feat: ka tbh

---
+++ app/checklist/page.tsx
@@ -0,0 +1,12 @@
+import { createClient } from "@/lib/supabase/server"
+
+export default async function Checklists(){
+    const supabase = await createClient()
+    const {data: {user}} = await supabase.auth.getUser()
+    console.log(user.id)
+    return(
+        <div className="these">
+            <p>bänis</p>
+        </div>
+    )
+}
