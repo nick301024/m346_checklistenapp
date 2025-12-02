bbb720e9fcd281f16f1a4e2cd79f2e809065bcd7
nick301024 <nick_bucher@sluz.ch>
2025-12-02 11:26:43 +0100

    fix: deploy

--- app/checklist/page.tsx
+++ app/checklist/page.tsx
@@ -3,10 +3,10 @@ import { createClient } from "@/lib/supabase/server"
 export default async function Checklists(){
     const supabase = await createClient()
     const {data: {user}} = await supabase.auth.getUser()
-    console.log(user.id)
+    console.log(user?.id)
     return(
         <div className="these">
-            <p>bänis</p>
+            <p>test</p>
         </div>
     )
 }
